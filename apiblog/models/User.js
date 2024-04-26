a2e86c707379f43adc2d34d748f959efd4ae60c7
Arkhopravo Sarkar <91507211+Arkhopravo@users.noreply.github.com>
2024-04-26 10:26:37 +0530

    Update User.js

--- apiblog/models/User.js
+++ apiblog/models/User.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 const {Schema, model} = mongoose
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true, min: 4, unique: true,},
+    username: {
+            type: String, 
+            required: true, 
+            min: 4, 
+            unique: true,
+    },
     email: {type: String, required: true, min: 4, unique: true,},
     password: {type: String, required: true},
     subscriptions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Subscription' }],
